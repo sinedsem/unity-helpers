9af6215d6ed8a0600d9d40c36d54a6d59a88316f
sinedsem <sinedsem@gmail.com>
2023-05-21 22:33:31 +0300

    fix many bugs in RecentSelections

--- Editor/RecentSelectionsWindow.cs
+++ Editor/RecentSelectionsWindow.cs
@@ -1,31 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+[Serializable]
+public class RecentSelection
+{
+    [field: SerializeField] public string Path { get; set; }
+    [field: SerializeField] public bool Scene { get; set; }
+    [field: SerializeField] public bool Pinned { get; set; }
+}
 
 public class RecentSelectionsWindow : EditorWindow
 {
-    private const int MaxHistory = 15;
+    private const int MaxHistory = 50;
 
     private Vector2 scrollPos;
-    private RecentSelections storage;
+
+    [SerializeField] private List<RecentSelection> values;
+
+    public static RecentSelectionsWindow Instance { get; private set; }
 
     private void OnEnable()
     {
-        storage = SoStorage.GetStorage<RecentSelections>();
+        if (Instance == null)
+            Instance = this;
         Selection.selectionChanged += HandleSelectionChange;
     }
 
     private void OnDisable()
     {
+        if (Instance == this)
+            Instance = null;
+
         Selection.selectionChanged -= HandleSelectionChange;
-        if (storage != null)
-        {
-            EditorUtility.SetDirty(storage);
-            AssetDatabase.SaveAssets();
-        }
     }
 
     private void HandleSelectionChange()
@@ -52,25 +64,30 @@ public class RecentSelectionsWindow : EditorWindow
             }
         }
 
-        var index = storage.Values.FindIndex(s => s.Scene == selection.Scene && s.Path == selection.Path);
+        AddToList(selection);
+    }
+
+    private void AddToList(RecentSelection selection)
+    {
+        var index = values.FindIndex(s => s.Scene == selection.Scene && s.Path == selection.Path);
         if (index >= 0)
         {
-            selection = storage.Values[index];
-            storage.Values.RemoveAt(index);
+            selection = values[index];
+            values.RemoveAt(index);
         }
 
-        storage.Values.Insert(0, selection);
+        values.Insert(0, selection);
 
         var i = 0;
-        while (storage.Values.Count > MaxHistory)
+        while (values.Count > MaxHistory)
         {
-            if (storage.Values[^(1 + i)].Pinned)
+            if (values[^(1 + i)].Pinned)
             {
                 i++;
             }
             else
             {
-                storage.Values.RemoveAt(storage.Values.Count - 1 - i);
+                values.RemoveAt(values.Count - 1 - i);
             }
         }
 
@@ -80,51 +97,31 @@ public class RecentSelectionsWindow : EditorWindow
 
     private void OnGUI()
     {
-        if (storage == null || storage.Values == null)
-        {
-            storage = SoStorage.GetStorage<RecentSelections>();
-            storage.Values ??= new List<RecentSelection>();
-        }
-
-        storage.Values.RemoveAll(o => o == null);
-
         scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
         var labelWidth = GUILayout.Width(20);
         var buttonWidth = GUILayout.Width(30);
-        for (var i = 0; i < storage.Values.Count; i++)
+        for (var i = 0; i < values.Count; i++)
         {
-            var recentSelection = storage.Values[i];
+            var recentSelection = values[i];
+
+            var obj = recentSelection.Scene
+                ? GetGameObjectAtPath(recentSelection.Path)
+                : AssetDatabase.LoadAssetAtPath<Object>(recentSelection.Path);
+            if (obj == null && !recentSelection.Pinned)
+                continue;
+
             EditorGUILayout.BeginHorizontal();
             GUILayout.Label(i.ToString(), labelWidth);
 
-            if (GUILayout.Button(recentSelection.Pinned ? "★" : "♠", buttonWidth))
+            if (GUILayout.Button(recentSelection.Pinned ? "★" : ".", buttonWidth))
             {
                 recentSelection.Pinned = !recentSelection.Pinned;
-            }
-
-
-            Object obj;
-            if (recentSelection.Scene)
-            {
-                obj = GetGameObjectAtPath(recentSelection.Path);
-            }
-            else
-            {
-                obj = AssetDatabase.LoadAssetAtPath<Object>(recentSelection.Path);
             }
 
             if (obj != null)
             {
                 EditorGUILayout.ObjectField(obj, typeof(Object), true);
 
-                var e = Event.current;
-                if (GUILayoutUtility.GetLastRect().Contains(e.mousePosition) && e.type == EventType.MouseDrag)
-                {
-                    DragAndDrop.PrepareStartDrag();
-                    DragAndDrop.objectReferences = new[] { obj };
-                    DragAndDrop.StartDrag("drag");
-                    Event.current.Use();
-                }
 
                 if (GUILayout.Button("▼", buttonWidth))
                 {
@@ -134,6 +131,17 @@ public class RecentSelectionsWindow : EditorWindow
                 if (GUILayout.Button("►", buttonWidth))
                 {
                     Selection.activeObject = obj;
+                }
+
+                var dragHandleWidth = GUILayout.Width(20);
+                EditorGUILayout.LabelField(" ≡ ", dragHandleWidth);
+                var e = Event.current;
+                if (GUILayoutUtility.GetLastRect().Contains(e.mousePosition) && e.type == EventType.MouseDrag)
+                {
+                    DragAndDrop.PrepareStartDrag();
+                    DragAndDrop.objectReferences = new[] { obj };
+                    DragAndDrop.StartDrag("drag");
+                    Event.current.Use();
                 }
             }
             else
@@ -150,8 +158,14 @@ public class RecentSelectionsWindow : EditorWindow
     public string GetGameObjectPath(GameObject obj)
     {
         var path = obj.name;
+
+        var currentPrefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+        var prefabRoot = currentPrefabStage != null ? currentPrefabStage.prefabContentsRoot : null;
+
         while (obj.transform.parent != null)
         {
+            if (prefabRoot && obj == prefabRoot)
+                break;
             obj = obj.transform.parent.gameObject;
             path = obj.name + "/" + path;
         }
@@ -196,6 +210,14 @@ public class RecentSelectionsWindow : EditorWindow
         return obj;
     }
 
+    private void OnCreateAsset(string assetPath)
+    {
+        AddToList(new RecentSelection
+        {
+            Path = assetPath
+        });
+    }
+
 
     [MenuItem("Tools/Recent Selections")]
     private static void ShowWindow()
@@ -203,5 +225,19 @@ public class RecentSelectionsWindow : EditorWindow
         var window = GetWindow<RecentSelectionsWindow>();
         window.titleContent = new GUIContent("Recent Selections");
         window.Show();
+    }
+
+    public class CustomAssetModificationProcessor : AssetModificationProcessor
+    {
+        static void OnWillCreateAsset(string assetPath)
+        {
+            Instance.OnCreateAsset(assetPath);
+        }
+
+        static AssetMoveResult OnWillMoveAsset(string sourcePath, string destinationPath)
+        {
+            Instance.OnCreateAsset(destinationPath);
+            return AssetMoveResult.DidNotMove;
+        }
     }
 }
